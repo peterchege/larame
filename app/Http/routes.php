d94a2e15645cdeeffcb3ee7d921d5868194019b0
peterkinuthia <peterchege442@gmail.com>
2019-07-24 21:26:22 +0300

    new route test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Hi about page";
+});
+
+Route::get('/contact', function () {
+    return "Hi am contact";
+});

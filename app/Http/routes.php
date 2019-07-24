6b7c59a5195def223233609f741f98c57a80f161
peterkinuthia <peterchege442@gmail.com>
2019-07-24 21:49:05 +0300

    pulling a url route using a shortname array

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,11 @@ Route::get('/post/{id}/{name}', function($id, $name){
 
     return "this is post number" . $id." ".$name;
 });
+
+Route::get('admin/posts/example', array('as'=>'admin.home' ,function(){
+
+    $url = route('admin.home');
+
+    return "this url is ". $url;
+
+} )); 

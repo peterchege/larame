88923b56b5c6e8e5c6168c25efff4809d2152188
peterkinuthia <peterchege442@gmail.com>
2019-08-22 06:57:26 +0300

    rollnback migration

--- database/migrations/2019_08_22_034508_create_posts_table.php
+++ database/migrations/2019_08_22_034508_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('tittle');
-            $table->text('body');
+            $table->text('content');
             $table->timestamps();
         });
     }

351d1c0017eabba442a8b43c78f97e2660e16a7a
peterkinuthia <peterchege442@gmail.com>
2019-08-22 06:52:19 +0300

    table post

--- database/migrations/2019_08_22_034508_create_posts_table.php
+++ database/migrations/2019_08_22_034508_create_posts_table.php
@@ -14,6 +14,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('tittle');
+            $table->text('body');
             $table->timestamps();
         });
     }
